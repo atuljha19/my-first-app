901dc8ffff58758cacf0ae4bf01ce6e66d3b0f23
atul jha <atuljha@atuls-MacBook-Pro.local>
2019-10-15 13:44:24 -0400

    Sample of how we can generate com. from cli and do its nesting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ServersComponent } from './servers/servers.component';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    ServersComponent,
     ServerComponent
   ],
   imports: [
